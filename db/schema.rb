7b580201a2982f42e2ee04e62ac2f534464bcfed
Mark Mounts <mark.mounts@dartmouth.edu>
2016-04-05 09:30:47 -0400

    add payment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329183454) do
+ActiveRecord::Schema.define(version: 20160405131815) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
